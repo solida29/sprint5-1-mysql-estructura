289b6dce825b291464e0f52ced030712016d456c
Solida <solida.var@gmail.com>
2023-12-23 10:50:06 +0100

    insert

---
+++ optica/optica_insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO optica.direcciones
+(calle, numero, piso, puerta, ciudad, codigo_postal, pais) VALUES 
+('Calle A', '10', 1, 2, 'Barcelona', '08001', 'España'),
+('Calle B', '20', 5, 1, 'Barcelona', '08002', 'España'),
+('Calle C', '30', 2, 1, 'Barcelona', '08003', 'España'),
+('Calle D', '40', 7, 6, 'Barcelona', '08004', 'España'),
+('Calle E', '50', 4, 2, 'Madrid', '08005', 'España'),
+('Calle F', '60', 2, 1, 'Madrid', '08006', 'España'),
+('Calle G', '70', 1, 1, 'Madrid', '08007', 'España'),
+('Calle H', '80', 2, 1, 'Madrid', '08008', 'España'),
+('Calle I', '90', 2, 1, 'San Sebastian', '08009', 'España'),
+('Calle J', '100', 2, 1, 'San Sebastian', '08010', 'España'),
+('Calle K', '110', 2, 1, 'San Sebastian', '08011', 'España'),
+('Calle L', '120', 2, 1, 'San Sebastian', '08012', 'España'),
+('Calle M', '130', 2, 1, 'Berlin', '08013', 'Alemania'),
+('Calle N', '140', 2, 1, 'Berlin', '08013', 'Alemania'),
+('Calle O', '150', 2, 1, 'Berlin', '08013', 'Alemania')
+;
