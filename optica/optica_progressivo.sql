bd6097163d3a9b4d7f65908411c87bb1694314e0
Solida <solida.var@gmail.com>
2023-12-23 15:59:17 +0100

    optica script insert ok

---
+++ optica/optica_progressivo.sql
@@ -0,0 +1,9 @@
+-- 8_ventas datos ---------------------
+INSERT INTO optica.ventas
+(fecha, gafas_id, cliente_id, vendedor_id) VALUES
+(current_timestamp(), 1, 1, 1),
+(current_timestamp(), 2, 1, 1),
+(current_timestamp(), 3, 2, 2),
+(current_timestamp(), 4, 2, 2),
+(current_timestamp(), 5, 3, 1)
+;
--- optica/optica_script.sql
+++ optica/optica_script.sql
@@ -14,7 +14,6 @@ CREATE TABLE IF NOT EXISTS optica.direcciones (
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-
 -- 2_proveedores definition ---------------
 CREATE TABLE IF NOT EXISTS optica.proveedores (
   id_proveedor INT(11) NOT NULL AUTO_INCREMENT,
@@ -29,21 +28,48 @@ CREATE TABLE IF NOT EXISTS optica.proveedores (
 ) 
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+-- 3_vendedores definition ---------------------
+CREATE TABLE IF NOT EXISTS optica.vendedores (
+  id_vendedor INT(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(100) NOT NULL,
+  apellido varchar(100) NOT NULL,
+  dni varchar(100) NOT NULL,
+  direccion_id int(11) DEFAULT NULL,
+  tiempo_ventas time DEFAULT NULL,
+  PRIMARY KEY (id_vendedor)
+  )
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- 3_gafas definition ---------------------
+-- 4_clientes definition ---------------------
+CREATE TABLE IF NOT EXISTS optica.clientes (
+  id_cliente INT(11) auto_increment NOT NULL,
+  nombre varchar(100) NOT NULL,
+  apellido varchar(100) NOT NULL,
+  direccion_id INT NULL,
+  telefono varchar(20) NOT NULL,
+  email varchar(100) NULL,
+  fecha_registro TIMESTAMP DEFAULT current_timestamp NOT NULL,
+  recomendacion_id INT NULL,
+  CONSTRAINT clientes_PK PRIMARY KEY (id_cliente),
+  CONSTRAINT clientes_FK_direccion FOREIGN KEY (direccion_id) REFERENCES direcciones(id_direccion),
+  CONSTRAINT clientes_FK_recomendacion FOREIGN KEY (recomendacion_id) REFERENCES clientes(id_cliente)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- 5_gafas definition ---------------------
 CREATE TABLE IF NOT EXISTS optica.gafas (
   id_gafas INT(11) auto_increment NOT NULL,
   marca varchar(100) NOT NULL,
   precio DECIMAL(10,2) NOT NULL,
   proveedor_id INT,
-  venta_id INT,
+  gafas_id INT,
   CONSTRAINT gafas_PK PRIMARY KEY (id_gafas),
-  CONSTRAINT gafas_proveedores_FK FOREIGN KEY (proveedor_id) REFERENCES proveedores(id_proveedor)
+  CONSTRAINT gafas_proveedores_FK FOREIGN KEY (proveedor_id) REFERENCES proveedores(id_proveedor),
+  CONSTRAINT gafas_FK_ventas FOREIGN KEY (gafas_id) REFERENCES gafas(id_gafas)
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-
--- 4_montura definition ---------------------
+-- 6_montura definition ---------------------
 CREATE TABLE IF NOT EXISTS optica.montura (
   material enum('pasta','metal','flotante') NOT NULL,
   color varchar(50) NOT NULL,
@@ -53,8 +79,7 @@ CREATE TABLE IF NOT EXISTS optica.montura (
 ) 
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-
--- 5_cristales definition ---------------------
+-- 7_cristales definition ---------------------
 CREATE TABLE IF NOT EXISTS optica.cristales (
   `izq_graduacion` float DEFAULT NULL,
   `izq_color` varchar(50) DEFAULT NULL,
@@ -66,8 +91,7 @@ CREATE TABLE IF NOT EXISTS optica.cristales (
 ) 
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-
--- 6_ventas definition ---------------------
+-- 8_ventas definition ---------------------
 CREATE TABLE IF NOT EXISTS optica.ventas (
 	id_venta INT(11) auto_increment NOT NULL,
 	fecha TIMESTAMP DEFAULT current_timestamp NOT NULL,
@@ -75,41 +99,8 @@ CREATE TABLE IF NOT EXISTS optica.ventas (
 	cliente_id INT NOT NULL,
 	vendedor_id INT NOT NULL,
 	CONSTRAINT ventas_PK PRIMARY KEY (id_venta),
-	CONSTRAINT ventas_FK FOREIGN KEY (gafas_id) REFERENCES gafas(id_gafas)
+	CONSTRAINT ventas_FK_gafas FOREIGN KEY (gafas_id) REFERENCES gafas(id_gafas),
+	CONSTRAINT ventas_FK_cliente FOREIGN KEY (cliente_id) REFERENCES clientes(id_cliente),
+  CONSTRAINT ventas_FK_vendedor FOREIGN KEY (vendedor_id) REFERENCES vendedores(id_vendedor)
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-
--- 7_clientes definition ---------------------
-CREATE TABLE IF NOT EXISTS optica.clientes (
-  id_cliente INT(11) auto_increment NOT NULL,
-  nombre varchar(100) NOT NULL,
-  apellido varchar(100) NOT NULL,
-  direccion_id INT NULL,
-  telefono varchar(20) NOT NULL,
-  email varchar(100) NULL,
-  fecha_registro TIMESTAMP DEFAULT current_timestamp NOT NULL,
-  recomendacion_id INT NULL,
-  venta_id INT NULL,
-  CONSTRAINT clientes_PK PRIMARY KEY (id_cliente),
-  CONSTRAINT clientes_FK_venta FOREIGN KEY (venta_id) REFERENCES ventas(id_venta),
-  CONSTRAINT clientes_FK_direccion FOREIGN KEY (direccion_id) REFERENCES direcciones(id_direccion),
-  CONSTRAINT clientes_FK_recomendacion FOREIGN KEY (recomendacion_id) REFERENCES clientes(id_cliente)
-)
-ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-
--- 8_vendedores definition ---------------------
-CREATE TABLE IF NOT EXISTS optica.vendedores (
-  `id_vendedor` INT(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `apellido` varchar(100) NOT NULL,
-  `dni` varchar(100) NOT NULL,
-  `direccion_id` int(11) DEFAULT NULL,
-  `venta_id` int(11) DEFAULT NULL,
-  `tiempo_ventas` time DEFAULT NULL,
-  CONSTRAINT vendedores_PK PRIMARY KEY (id_vendedor),
-  CONSTRAINT vendedores_FK_venta FOREIGN KEY (venta_id) REFERENCES ventas(id_venta)
-  )
-ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
