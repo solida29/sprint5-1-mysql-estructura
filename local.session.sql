3ab4ff815a32384bc6efa69845aa6a664cada183
Solida <solida.var@gmail.com>
2023-12-26 18:43:40 +0100

    optica_consultas.sql OK

--- local.session.sql
+++
@@ -1,7 +0,0 @@
--- Llista les diferents ulleres que ha venut un empleat durant un any.
-
-SELECT gafas.marca, ventas.fecha, vendedores.nombre
-FROM optica.gafas
-LEFT JOIN optica.ventas ON gafas.id_gafas = ventas.gafas_id
-LEFT JOIN optica.vendedores ON vendedores.id_vendedor = ventas.vendedor_id
-WHERE YEAR(ventas.fecha) = YEAR(CURDATE());
--- optica/optica_consultas.sql
+++ optica/optica_consultas.sql
@@ -5,18 +5,17 @@ SELECT COUNT(cliente_id) AS total_ventas
 FROM optica.ventas
 WHERE cliente_id = 3;
 
--- Llista les diferents ulleres que ha venut un empleat durant un any.
-SELECT gafas.marca, ventas.fecha, vendedores.nombre
-FROM optica.gafas
-LEFT JOIN optica.ventas ON gafas.id_gafas = ventas.gafas_id
-LEFT JOIN optica.vendedores ON vendedores.id_vendedor = ventas.vendedor_id
-WHERE YEAR(ventas.fecha) = YEAR(CURDATE());
+-- Llista les diferents ulleres que ha venut un empleat durant un any
+SELECT vendedores.nombre, gafas.marca, ventas.fecha
+FROM optica.vendedores
+LEFT JOIN optica.ventas ON vendedores.id_vendedor = ventas.vendedor_id
+LEFT JOIN optica.gafas ON ventas.gafas_id = gafas.id_gafas
+WHERE YEAR(ventas.fecha) = YEAR(CURDATE()) -- YEAR(CURDATE()) es para el año en curso, sino se pone el año directo
+ORDER BY vendedores.nombre ASC; -- ordenar por los nombres de los vendedores de manera ascendente
 
 -- Llista els diferents proveïdors que han subministrat ulleres venudes amb èxit per l'òptica.
-SELECT proveedores.nombre, COUNT(ventas.id_venta) AS total_ventas
+SELECT proveedores.nombre, gafas.marca, ventas.fecha
 FROM optica.proveedores
-INNER JOIN optica.gafas
-ON proveedores.id_proveedor = gafas.proveedor_id
-INNER JOIN optica.ventas
-ON gafas.id_gafas = ventas.gafas_id
-GROUP BY proveedores.nombre;
+LEFT JOIN optica.gafas ON proveedores.id_proveedor = gafas.proveedor_id
+INNER JOIN optica.ventas ON ventas.gafas_id = gafas.id_gafas
+ORDER BY proveedores.nombre ASC;
