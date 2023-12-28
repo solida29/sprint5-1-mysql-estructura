03615616d359e96d8200fa3bbae6632965b49c85
Solida <solida.var@gmail.com>
2023-12-29 00:12:46 +0100

    pizzeria_insert.sql

---
+++ pizzeria/pizzeria_insert.sql
@@ -0,0 +1,6 @@
+-- 1_direcciones datos -----------
+INSERT INTO pizzeria.direcciones
+(direccion, codigo_postal, ciudad, provincia) VALUES
+('calle piscina, 12, 1-3', 08001, 'Barcelona', 'Barcelona'),
+('calle pasarela, 244, 6-2', 08002, 'Barcelona', 'Barcelona')
+;
