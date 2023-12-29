5f1f2d02bd2c4e78b300247b6dd791c412cdea20
Solida <solida.var@gmail.com>
2023-12-29 11:44:32 +0100

    cambios diagrama

--- pizzeria/pizzeria_script.sql
+++ pizzeria/pizzeria_script.sql
@@ -14,10 +14,10 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 -- 2_clientes definition -----------
 CREATE TABLE IF NOT EXISTS pizzeria.clientes (
     id_cliente INT(11) auto_increment NOT NULL,
-    nombre_cliente VARCHAR(100),
-    apellido_cliente VARCHAR(100),
-    direccion_id INT(11),
-    telefono_cliente VARCHAR(20),
+    nombre_cliente VARCHAR(100) NOT NULL,
+    apellido_cliente VARCHAR(100) NOT NULL,
+    direccion_id INT(11) NULL,
+    telefono_cliente VARCHAR(20) NOT NULL,
     PRIMARY KEY (id_cliente),
     CONSTRAINT clientes_FK_direccion 
     FOREIGN KEY (direccion_id) REFERENCES direcciones (id_direccion)
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS pizzeria.pedidos (
     id_pedido INT(11) auto_increment NOT NULL,
     cliente_id INT(11),
     fecha_reparto TIMESTAMP DEFAULT current_timestamp,
-    precio_total DECIMAL (10,2),
+    precio_total DECIMAL (10,2) NULL,
     PRIMARY KEY (id_pedido),
     CONSTRAINT pedidos_FK_cliente
     FOREIGN KEY (cliente_id) REFERENCES clientes (id_cliente)
@@ -75,7 +75,7 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 CREATE TABLE IF NOT EXISTS pizzeria.tiendas (
     id_tienda INT(11) auto_increment NOT NULL,
     direccion_id INT(11),
-    pedido_id INT(11),
+    pedido_id INT(11) NULL,
     PRIMARY KEY (id_tienda),
     CONSTRAINT tiendas_FK_direccion FOREIGN KEY (direccion_id) REFERENCES direcciones(id_direccion),
     CONSTRAINT tiendas_FK_pedido FOREIGN KEY (pedido_id) REFERENCES pedidos(id_pedido)
@@ -87,8 +87,8 @@ CREATE TABLE IF NOT EXISTS pizzeria.empleados (
     id_empleado INT(11) auto_increment NOT NULL,
     nombre_empleado VARCHAR(100) NOT NULL,
     apellido_empleado VARCHAR(100) NOT NULL,
-    nif VARCHAR(50),
-    telefono VARCHAR(50),
+    nif VARCHAR(50) UNIQUE,
+    telefono VARCHAR(50) NULL,
     puesto ENUM('cocinero', 'repartidor'),
     tienda_id INT(11),
     PRIMARY KEY (id_empleado),
@@ -99,11 +99,12 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 -- 9_reparto_domicilio definition -----------
 CREATE TABLE IF NOT EXISTS pizzeria.repartos_domicilio (
     id_reparto INT(11) auto_increment NOT NULL,
-    empleado_id INT(11),
     pedido_id INT(11),
+    empleado_id INT(11),
     fecha_hora_entrega TIMESTAMP DEFAULT current_timestamp,
     PRIMARY KEY (id_reparto),
-    CONSTRAINT rep_domicilio_FK_empleado FOREIGN KEY (empleado_id) REFERENCES empleados(id_empleado),
-    CONSTRAINT rep_domicilio_FK_pedido FOREIGN KEY (pedido_id) REFERENCES pedidos(id_pedido)
+    CONSTRAINT rep_domicilio_FK_pedido FOREIGN KEY (pedido_id) REFERENCES pedidos(id_pedido),
+    CONSTRAINT rep_domicilio_FK_empleado FOREIGN KEY (empleado_id) REFERENCES empleados(id_empleado)
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
